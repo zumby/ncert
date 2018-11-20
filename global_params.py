99c27e102380db2bdf9cf6108fa6278b86a559fb
bzumby <benzumby@gmail.com>
2018-11-20 22:22:24 +0200

    a few comments got removed

--- global_params.py
+++ global_params.py
@@ -2,13 +2,12 @@ import yaml
 
 class GlobalParams(object):
     """Working with global API params"""
-    # url = "https://api.sandbox.namecheap.com/xml.response" then you can do like GlobalParams.url to call url value
     def __init__(self):
-        # self.url = "https://api.sandbox.namecheap.com/xml.response" # think of putting this to yaml
         self.url = self.get_global_params()["URL"]
         self.params = self.get_global_params()["GlobalParams"]
 
     def get_global_params(self):
+        """Returns the dict with th GlobalParams and NC API URL"""
         with open('nc_api_config.yaml', 'r') as stream:
             params_dict = yaml.load(stream)
             # return config['GlobalParams'], config['URL']
